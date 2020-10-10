87d9b67abc2957aa4497eec794a47498b7bf70d7
Ricardo Soto Estévez <uo265710@uniovi.es>
2020-10-10 21:01:42 +0200

    Fix vsc.xml error

--- app/src/main/java/es/uniovi/eii/cows/model/NewsItem.java
+++ app/src/main/java/es/uniovi/eii/cows/model/NewsItem.java
@@ -11,93 +11,93 @@ import java.util.Objects;
  */
 public class NewsItem {
 
-    private String title;
-    private String description;
-    private String link;                                // News link
-    private LocalDateTime date;                         // Publication date
-    private String source;                              // Source of the news
-    private String imageUrl;                            // URL of the image of the news
-    private String fallbackImage;                       // Image to use when no image
+	private String title;
+	private String description;
+	private String link;                                // News link
+	private LocalDateTime date;                         // Publication date
+	private String source;                              // Source of the news
+	private String imageUrl;                            // URL of the image of the news
+	private String fallbackImage;                       // Image to use when no image
 
-    public NewsItem() {
-    }
+	public NewsItem() {
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public String getLink() {
-        return link;
-    }
+	public String getLink() {
+		return link;
+	}
 
-    public void setLink(String link) {
-        this.link = link;
-    }
+	public void setLink(String link) {
+		this.link = link;
+	}
 
-    public String getSource() {
-        return source;
-    }
+	public String getSource() {
+		return source;
+	}
 
-    public void setSource(String source) {
-        this.source = source;
-    }
+	public void setSource(String source) {
+		this.source = source;
+	}
 
-    public String getImageUrl() {
-        if (imageUrl == null || imageUrl.isEmpty())
-            return fallbackImage;
-        return imageUrl;
-    }
+	public String getImageUrl() {
+		if (imageUrl == null || imageUrl.isEmpty())
+			return fallbackImage;
+		return imageUrl;
+	}
 
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
 
-    public String getFallbackImage() {
-        return fallbackImage;
-    }
+	public String getFallbackImage() {
+		return fallbackImage;
+	}
 
-    public void setFallbackImage(String fallbackImage) {
-        this.fallbackImage = fallbackImage;
-    }
+	public void setFallbackImage(String fallbackImage) {
+		this.fallbackImage = fallbackImage;
+	}
 
-    public LocalDateTime getDate() {
-        return date;
-    }
+	public LocalDateTime getDate() {
+		return date;
+	}
 
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        NewsItem newsItem = (NewsItem) o;
-        // TODO algorithm to recognise similar news
-        return link.equals(newsItem.link);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		NewsItem newsItem = (NewsItem) o;
+		// TODO algorithm to recognise similar news
+		return link.equals(newsItem.link);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(link);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(link);
+	}
 
-    @Override
-    @NonNull
-    public String toString() {
-        return "NewsItem{ " + title + " - " + source +
-                ", [" + link + "] }";
-    }
+	@Override
+	@NonNull
+	public String toString() {
+		return "NewsItem{ " + title + " - " + source +
+				", [" + link + "] }";
+	}
 }
