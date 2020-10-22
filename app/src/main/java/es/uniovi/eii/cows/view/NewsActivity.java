e6a19b151fed8c9303db0934b0aad2972945f3d6
Adri-San <adrianvadrianv@outlook.com>
2020-10-22 18:17:59 +0100

    News Activity: Making NewsItem Parcelable, launching NewsActivity from MainActivity and triggering onClick

--- app/src/main/java/es/uniovi/eii/cows/view/NewsActivity.java
+++ app/src/main/java/es/uniovi/eii/cows/view/NewsActivity.java
@@ -1,18 +1,26 @@
 package es.uniovi.eii.cows.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import es.uniovi.eii.cows.R;
+import es.uniovi.eii.cows.model.NewsItem;
 
 public class NewsActivity extends AppCompatActivity {
+
+    private NewsItem newsItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
-        setSupportActionBar(findViewById(R.id.app_bar));
+        //setSupportActionBar(findViewById(R.id.app_bar));
+
+        //Receiving intent
+        Intent newsIntent = getIntent();
+        newsItem = newsIntent.getParcelableExtra(MainActivity.SELECTED_NEWS_ITEM);
+
     }
 }
