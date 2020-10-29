f990ec4b1d4deb6650d4e73ab1e950f6ae1d97e6
Ricardo Soto Estévez <uo265710@uniovi.es>
2020-10-30 00:22:12 +0100

    Improve filter evaluation

--- app/src/main/java/es/uniovi/eii/cows/controller/reader/filter/CovidFilter.java
+++ app/src/main/java/es/uniovi/eii/cows/controller/reader/filter/CovidFilter.java
@@ -9,34 +9,49 @@ import es.uniovi.eii.cows.model.NewsItem;
 
 public class CovidFilter {
 
-	public static String[] relatedTerms = {
+	public static final int THRESHOLD = 7;
+
+	public static String[] topTerms = {
 			"covid",
 			"covid-19",
 			"coronavirus",
 			"sars-cov-2"
 	};
 
+	public static String[] relatedTerms = {
+			"pandemia",
+			"alarma",
+			"confinamiento",
+			"pcr",
+			"restricciones",
+			"mascarilla"
+	};
+
 	public static void evaluate(NewsItem item) {
 		if (!item.isCovidRelated()) {
-			long value = 0;
-			value += evaluateString(item.getTitle()) * 2;
-			value += evaluateString(item.getDescription());
-			item.setCovidRelated(value > 3);
-			Log.d("VALUE", String.valueOf(value));
+			long h = 0;
+			h += evaluateString(item.getTitle()) * 2;
+			h += evaluateString(item.getDescription());
+			item.setCovidRelated(h > THRESHOLD);
+			if (h > 0)	Log.d("Evaluation", "[" + h + "] " + item.getTitle());
 		}
-	}
-
-	public static boolean filter(NewsItem item) {
-		return item.isCovidRelated();
 	}
 
 	private static long evaluateString(String s) {
 		String[] words = s.trim().toLowerCase().split(" ");
-		AtomicLong value = new AtomicLong(0);
+		AtomicLong top = new AtomicLong(0);
+		Arrays.stream(topTerms)
+				.map(t-> Arrays.stream(words).filter(w -> w.equals(t)).count())
+				.reduce(Long::sum).ifPresent(top::set);
+		AtomicLong related = new AtomicLong(0);
 		Arrays.stream(relatedTerms)
 				.map(t-> Arrays.stream(words).filter(w -> w.equals(t)).count())
-				.reduce(Long::sum).ifPresent(value::set);
-		return value.get();
+				.reduce(Long::sum).ifPresent(related::set);
+		return top.get() * 5 + related.get();
+	}
+
+	public static boolean filter(NewsItem item) {
+		return item.isCovidRelated();
 	}
 
 }
