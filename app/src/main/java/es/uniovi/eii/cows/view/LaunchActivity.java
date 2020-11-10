f1489fef2deaed04377dd53b96f24b221ee20554
Andrea López Suárez <uo264703@uniovi.es>
2020-11-10 21:53:12 +0100

    Create Splash Screen drawable and new style for it

---
+++ app/src/main/java/es/uniovi/eii/cows/view/LaunchActivity.java
@@ -0,0 +1,10 @@
+package es.uniovi.eii.cows.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LaunchActivity  extends AppCompatActivity {
+
+}
