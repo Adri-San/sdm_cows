f36c12fad575c9b89defd8cda04c7807f7bd24b8
Andrea López Suárez <uo264703@uniovi.es>
2021-01-07 16:24:31 +0100

    Add test for scrolling

--- app/src/androidTest/java/es/uniovi/eii/cows/view/MainActivityTest.java
+++ app/src/androidTest/java/es/uniovi/eii/cows/view/MainActivityTest.java
@@ -14,9 +14,13 @@ import es.uniovi.eii.cows.R;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.swipeDown;
+import static androidx.test.espresso.action.ViewActions.swipeUp;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static es.uniovi.eii.cows.MatcherManager.firstItem;
 import static es.uniovi.eii.cows.MatcherManager.getText;
 import static org.hamcrest.Matchers.equalToIgnoringCase;
@@ -38,5 +42,18 @@ public class MainActivityTest {
         recyclerView.perform(actionOnItemAtPosition(0, click()));
         // Assert that item has the title
         assertThat(getText(withId(R.id.idTitle_news)), equalToIgnoringCase(titleNews0));
+    }
+
+    @Test
+    public void testScroll() {
+        try { Thread.sleep(3000); } catch (InterruptedException e) { e.printStackTrace(); }
+
+        ViewInteraction recyclerView = onView(withId(R.id.idRecycler_main));
+        // Get title of first news item
+        String titleNews0 = getText(firstItem(withId(R.id.idTitle)));
+        // Try to scroll down
+        recyclerView.perform(swipeUp());
+        // Check that first item is not visible
+        onView(withText(titleNews0)).check(doesNotExist());
     }
 }
