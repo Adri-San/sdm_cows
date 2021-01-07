cf6c6852e96724f7e07802cbe3e2026219434a65
Andrea López Suárez <uo264703@uniovi.es>
2021-01-07 15:05:04 +0100

    Create class MatcherManager to encapsulate new matchers

--- app/src/androidTest/java/es/uniovi/eii/cows/view/MainActivityTest.java
+++ app/src/androidTest/java/es/uniovi/eii/cows/view/MainActivityTest.java
@@ -1,22 +1,11 @@
 package es.uniovi.eii.cows.view;
 
 
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-import android.widget.TextView;
-
-import androidx.test.espresso.UiController;
-import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,9 +16,9 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
-import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.Matchers.allOf;
+import static es.uniovi.eii.cows.MatcherManager.firstItem;
+import static es.uniovi.eii.cows.MatcherManager.getText;
 import static org.hamcrest.Matchers.equalToIgnoringCase;
 
 @LargeTest
@@ -40,11 +29,7 @@ public class MainActivityTest {
 
     @Test
     public void testClickFirst() {
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        try { Thread.sleep(3000); } catch (InterruptedException e) { e.printStackTrace(); }
 
         ViewInteraction recyclerView = onView(withId(R.id.idRecycler_main));
         // Get title of first news item
@@ -53,67 +38,5 @@ public class MainActivityTest {
         recyclerView.perform(actionOnItemAtPosition(0, click()));
         // Assert that item has the title
         assertThat(getText(withId(R.id.idTitle_news)), equalToIgnoringCase(titleNews0));
-    }
-
-    private String getText(final Matcher<View> matcher) {
-        final String[] stringHolder = { null };
-        onView(matcher).perform(new ViewAction() {
-            @Override
-            public Matcher<View> getConstraints() {
-                return isAssignableFrom(TextView.class);
-            }
-
-            @Override
-            public String getDescription() {
-                return "getting text from a TextView";
-            }
-
-            @Override
-            public void perform(UiController uiController, View view) {
-                TextView tv = (TextView)view; //Save, because of check in getConstraints()
-                stringHolder[0] = tv.getText().toString();
-            }
-        });
-        return stringHolder[0];
-    }
-
-    private <T> Matcher<T> firstItem(final Matcher<T> matcher) {
-        return new BaseMatcher<T>() {
-            boolean isFirst = true;
-
-            @Override
-            public boolean matches(final Object item) {
-                if (isFirst && matcher.matches(item)) {
-                    isFirst = false;
-                    return true;
-                }
-
-                return false;
-            }
-
-            @Override
-            public void describeTo(final Description description) {
-                description.appendText("should return first matching item");
-            }
-        };
-    }
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
     }
 }
--- app/src/androidTest/java/es/uniovi/eii/cows/view/SavedActivityTest.java
+++ app/src/androidTest/java/es/uniovi/eii/cows/view/SavedActivityTest.java
@@ -1,31 +1,21 @@
 package es.uniovi.eii.cows.view;
 
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 
 import es.uniovi.eii.cows.R;
-import es.uniovi.eii.cows.view.adapter.NewsAdapter;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static es.uniovi.eii.cows.MatcherManager.firstItem;
 import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
@@ -77,46 +67,5 @@ public class SavedActivityTest {
         savedNavDrawer.perform(click());
 
          */
-    }
-
-
-    private <T> Matcher<T> firstItem(final Matcher<T> matcher) {
-        return new BaseMatcher<T>() {
-            boolean isFirst = true;
-
-            @Override
-            public boolean matches(final Object item) {
-                if (isFirst && matcher.matches(item)) {
-                    isFirst = false;
-                    return true;
-                }
-
-                return false;
-            }
-
-            @Override
-            public void describeTo(final Description description) {
-                description.appendText("should return first matching item");
-            }
-        };
-    }
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
     }
 }
