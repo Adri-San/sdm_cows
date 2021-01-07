d4307a47f1df4960e2ff6507796d0526f2095996
Andrea López Suárez <uo264703@uniovi.es>
2021-01-07 14:50:00 +0100

    Add test: click on first news-item on MainActivity

--- app/src/androidTest/java/es/uniovi/eii/cows/view/MainActivityTest.java
+++ app/src/androidTest/java/es/uniovi/eii/cows/view/MainActivityTest.java
@@ -4,12 +4,16 @@ package es.uniovi.eii.cows.view;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.widget.TextView;
 
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
+import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
@@ -22,8 +26,11 @@ import es.uniovi.eii.cows.R;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -32,15 +39,63 @@ public class MainActivityTest {
     public ActivityScenarioRule<LaunchActivity> mActivityTestRule = new ActivityScenarioRule<>(LaunchActivity.class);
 
     @Test
-    public void autoLogIn() {
+    public void testClickFirst() {
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        ViewInteraction recyclerView = onView(
-                allOf(withId(R.id.idRecycler_main)));
+
+        ViewInteraction recyclerView = onView(withId(R.id.idRecycler_main));
+        // Get title of first news item
+        String titleNews0 = getText(firstItem(withId(R.id.idTitle)));
+        // Click on first news item
         recyclerView.perform(actionOnItemAtPosition(0, click()));
+        // Assert that item has the title
+        assertThat(getText(withId(R.id.idTitle_news)), equalToIgnoringCase(titleNews0));
+    }
+
+    private String getText(final Matcher<View> matcher) {
+        final String[] stringHolder = { null };
+        onView(matcher).perform(new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isAssignableFrom(TextView.class);
+            }
+
+            @Override
+            public String getDescription() {
+                return "getting text from a TextView";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                TextView tv = (TextView)view; //Save, because of check in getConstraints()
+                stringHolder[0] = tv.getText().toString();
+            }
+        });
+        return stringHolder[0];
+    }
+
+    private <T> Matcher<T> firstItem(final Matcher<T> matcher) {
+        return new BaseMatcher<T>() {
+            boolean isFirst = true;
+
+            @Override
+            public boolean matches(final Object item) {
+                if (isFirst && matcher.matches(item)) {
+                    isFirst = false;
+                    return true;
+                }
+
+                return false;
+            }
+
+            @Override
+            public void describeTo(final Description description) {
+                description.appendText("should return first matching item");
+            }
+        };
     }
 
     private static Matcher<View> childAtPosition(
