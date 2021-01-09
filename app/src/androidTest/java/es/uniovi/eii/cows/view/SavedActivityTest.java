c6c937c45c6b6d66272509ab8f098c32c33d2dfb
Andrea López Suárez <uo264703@uniovi.es>
2021-01-09 15:48:00 +0100

    Change savedActivityTest

--- app/src/androidTest/java/es/uniovi/eii/cows/view/SavedActivityTest.java
+++ app/src/androidTest/java/es/uniovi/eii/cows/view/SavedActivityTest.java
@@ -1,34 +1,26 @@
 package es.uniovi.eii.cows.view;
 
-import android.util.Log;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.MockitoAnnotations;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import es.uniovi.eii.cows.R;
-import es.uniovi.eii.cows.data.helper.FirebaseHelper;
-import es.uniovi.eii.cows.model.NewsItem;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static es.uniovi.eii.cows.MatcherManager.firstItem;
 import static es.uniovi.eii.cows.MatcherManager.getText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.equalToIgnoringCase;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -49,6 +41,6 @@ public class SavedActivityTest {
         onView(allOf(withId(R.id.nav_saved), isDisplayed())).perform(click());
         try { Thread.sleep(3000); } catch (InterruptedException e) { e.printStackTrace(); }
         // Assert that the news item is saved
-        assertThat(getText(firstItem(withId(R.id.idTitle))), equalToIgnoringCase(titleNews0));
+        onView(withText(titleNews0)).check(matches(isDisplayed()));
     }
 }
